a6ba4a5360afdd8a6058b524ff78257a774243f4
Mark Marzan <markamarzan@gmail.com>
2022-05-03 21:38:04 -1000

    Add data sets for workout categories by muscle group

---
+++ script.js
@@ -0,0 +1,5 @@
+//Data sets for workout categories by muscle group
+const fullBody = ['Jumping Jacks', 'Burpees', 'Kettlebell Swings', 'High Knees', 'Jump Rope'];
+const upperBody = ['Rope Whips', 'Push Ups', 'Bear Crawls', 'Up-Down Plank'];
+const lowerBody = ['Squat Jump', 'Lunge Jump', 'Tuck Jump', 'Reverse Lunges'];
+const core = ['Planks', 'Runner Crunches', 'Mountain Climbers', 'Russian Twists'];
