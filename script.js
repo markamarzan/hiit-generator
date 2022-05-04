fb48942048ecea8429b62fed40b03fc73a185b6c
Mark Marzan <markamarzan@gmail.com>
2022-05-03 21:54:42 -1000

    Move workout categories into object

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //Data sets for workout categories by muscle group
-const fullBody = ['Jumping Jacks', 'Burpees', 'Kettlebell Swings', 'High Knees', 'Jump Rope'];
-const upperBody = ['Rope Whips', 'Push Ups', 'Bear Crawls', 'Up-Down Plank'];
-const lowerBody = ['Squat Jump', 'Lunge Jump', 'Tuck Jump', 'Reverse Lunges'];
-const core = ['Planks', 'Runner Crunches', 'Mountain Climbers', 'Russian Twists'];
+const exercises = {
+  fullBody: ['Jumping Jacks', 'Burpees', 'Kettlebell Swings', 'High Knees', 'Jump Rope'],
+  upperBody: ['Rope Whips', 'Push Ups', 'Bear Crawls', 'Up-Down Plank'],
+  lowerBody: ['Squat Jump', 'Lunge Jump', 'Tuck Jump', 'Reverse Lunges'],
+  core: ['Planks', 'Runner Crunches', 'Mountain Climbers', 'Russian Twists']
+}
 
 //Random number generator
 function getRandomNum(num) {
