2e739f83656c0812fb01c461b1ebf2756cb17f7a
Mark Marzan <markamarzan@gmail.com>
2022-05-03 21:40:43 -1000

    Add random number generator function

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const fullBody = ['Jumping Jacks', 'Burpees', 'Kettlebell Swings', 'High Knees',
 const upperBody = ['Rope Whips', 'Push Ups', 'Bear Crawls', 'Up-Down Plank'];
 const lowerBody = ['Squat Jump', 'Lunge Jump', 'Tuck Jump', 'Reverse Lunges'];
 const core = ['Planks', 'Runner Crunches', 'Mountain Climbers', 'Russian Twists'];
+
+//Random number generator
+function getRandomNum(num) {
+  return Math.floor(Math.random() * num);
+}
+
