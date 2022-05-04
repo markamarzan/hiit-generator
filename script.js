33ec6d1400fb2e6ffefcd4d06ff9f4cc3e0762d3
Mark Marzan <markamarzan@gmail.com>
2022-05-04 00:07:16 -1000

    Add Generator Function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Data sets for workout categories by muscle group
 const exercises = {
-  fullBody: ['Jumping Jacks', 'Burpees', 'Kettlebell Swings', 'High Knees', 'Jump Rope'],
-  upperBody: ['Rope Whips', 'Push Ups', 'Bear Crawls', 'Up-Down Plank'],
-  lowerBody: ['Squat Jump', 'Lunge Jump', 'Tuck Jump', 'Reverse Lunges'],
-  core: ['Planks', 'Runner Crunches', 'Mountain Climbers', 'Russian Twists']
+  'Full Body': ['Jumping Jacks', 'Burpees', 'Kettlebell Swings', 'High Knees', 'Jump Rope'],
+  'Upper Body': ['Rope Whips', 'Push Ups', 'Bear Crawls', 'Up-Down Plank'],
+  'Lower Body': ['Squat Jump', 'Lunge Jump', 'Tuck Jump', 'Reverse Lunges'],
+  'Core': ['Planks', 'Runner Crunches', 'Mountain Climbers', 'Russian Twists']
 }
 
 //Random number generator
@@ -11,3 +11,13 @@ function getRandomNum(num) {
   return Math.floor(Math.random() * num);
 }
 
+//HIIT Generator Function
+function generateHIIT(arr) {
+  let hiitWorkout = [];
+  for (let prop in arr) {
+    hiitWorkout.push(`${prop}: ${arr[prop][getRandomNum(arr[prop].length)]}`);
+  }
+  console.log(hiitWorkout);
+}
+
+generateHIIT(exercises);
